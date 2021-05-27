1a3d23f8280b3243ddb0df929aeeb045123e9207
Shreyansh Dash <79135001+shreyansh-dash@users.noreply.github.com>
2021-05-27 15:48:10 +0530

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,28 @@
+import random
+
+# Take input form the user
+user_action = input("Enter a choice (rock, paper, scissors): ")
+
+# Giving the computer choices to make and prvide a random output by itself
+possible_actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
+print(f"\nYou chose {user_action}, computer chose {computer_action}.\n")
+
+# Conditions to compare the user and the computer's input 
+if user_action == computer_action:
+    print(f"Both players selected {user_action}. It's a tie!")
+elif user_action == "rock":
+    if computer_action == "scissors":
+        print("Rock smashes scissors! You win!")
+    else:
+        print("Paper covers rock! You lose.")
+elif user_action == "paper":
+    if computer_action == "rock":
+        print("Paper covers rock! You win!")
+    else:
+        print("Scissors cuts paper! You lose.")
+elif user_action == "scissors":
+    if computer_action == "paper":
+        print("Scissors cuts paper! You win!")
+    else:
+        print("Rock smashes scissors! You lose.")
